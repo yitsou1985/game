84fc0127f7b79f9feb0a6b17b235e4f203e7d1c8
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2019-11-26 22:13:34 +0200

    Add files via upload

---
+++ arrayex.js
@@ -0,0 +1,10 @@
+//alert("cool");
+var array = ["banana","apples","oranges","bluberrries"];
+// array.shift("banana");//remove banana
+// array.sort()//Sort the array in order
+//array.push("kiwi")at the end of the array.
+//array.splice(1,1)//remome apples
+//array.reverse()
+//array.splice(2,0,"kiwi")
+// var array2 = ["banana",["apples"],["oranges"],"bbluberrries"]]; //access to oranges
+// array2[1][1]
